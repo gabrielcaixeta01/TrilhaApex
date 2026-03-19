6b6cfc4ffe830fb76bb51416438bf5293d64559b
Gabriel Caixeta <gabrielcaixetaromero@gmail.com>
2026-03-19 15:31:26 -0300

    Endpoints gerais montados, começar a arrumar os bugs

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel, Field
 
+class UserSchema(BaseModel):
+    id: int = Field(...)
+    username: str = Field(...)
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    email: str = Field(...)
+    password: str = Field(...)
+    phone: str = Field(...)
+    userStatus: int = Field(...)
+
