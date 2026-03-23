7c6fe179f3802f12abb8665a0903db24aa3ade92
Gabriel Caixeta <gabrielcaixetaromero@gmail.com>
2026-03-23 15:50:51 -0300

    Adicionando migracoes com Alembic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from app.database import Base, engine
 from app.routers import pet_crud, order_crud, user_crud
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Petstore da Apex")
 
